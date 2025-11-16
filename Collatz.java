b3cfd9f21a4f0f28db0ea4a295ff5ebd4ed809f4
YamlRUNI <155106022+YamlRUNI@users.noreply.github.com>
2025-11-16 14:31:32 +0200

    Feedback Collatz.java

--- Collatz.java
+++ Collatz.java
@@ -1,6 +1,19 @@
+/*
+Feedback:
+
+Hi Roni,
+I see you haven't completed the task in this file.
+What was the obstacle?
+Can I offer any help?
+Yam
+*/
+
+
+
 // Demonstrates the Collatz conjecture.
 public class Collatz {
 	public static void main(String args[]) {
 	    // Replace this comment with your code
 	}
 }
+
